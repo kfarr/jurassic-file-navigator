9163a05c8a0e44e3d1e1300c42f96c5c29b25532
Nathan White <nw@nwhite.net>
2016-03-21 21:41:26 -0700

    app loader

--- app.js
+++ app.js
@@ -1,75 +1,30 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-var swig = require('swig');
-var favicons = require('connect-favicons');
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
-
-var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-
-app.engine('html', swig.renderFile);
-app.set('view engine', 'html');
-
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-
-
-app.use(favicons(__dirname + '/public/images/icons'));
-
-// allowing for many include possibilities
-app.use('/scripts/aframe', express.static(__dirname + './node_modules/aframe/'));
-app.use('/scripts/aframe/dist', express.static(__dirname + '/node_modules/aframe/dist/'));
-app.use('/scripts/aframe-text-component/dist', express.static(__dirname + '/node_modules/aframe-text-component/dist/'));
-
-
-
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', routes);
-app.use('/users', users);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
-});
-
-// error handlers
-
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-      message: err.message,
-      error: err
-    });
-  });
-}
-
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
-});
+var boot = module.exports = require('./boot')
+  , app = boot(require('express')())
+  , server = require('http').createServer(app);
 
 
 module.exports = app;
+
+/**
+ * Load app directly if no parent is set.
+ */
+if (!module.parent) {
+
+  process.addListener('uncaughtException', function(err){
+    console.error('Uncaught exception!');
+    console.error(err.stack || err);
+    process.exit(1);
+  });
+  
+  server.listen(app.set('port'), function(){
+    console.error('\x1b[32m'+ app.set('app name') +'\x1b[0m running on http://%s:%d',
+      this.address().address.replace(/:/g, '') || 'localhost',
+      this.address().port);
+  });
+
+}
+
+
+
+
