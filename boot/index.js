a355844a16dc58572c56fb376867b4c574e8d8d1
Nathan White <nw@nwhite.net>
2016-03-21 21:22:46 -0700

    boot loader

---
+++ boot/index.js
@@ -0,0 +1,14 @@
+var config = require('./config')
+  , error_handlers = require('../lib/util/error-handlers')
+  , routes = require('../routes');
+
+module.exports = function boot(app) {
+  
+  config(app);
+  
+  routes(app);
+  
+  error_handlers(app);
+  
+  return app;
+}
